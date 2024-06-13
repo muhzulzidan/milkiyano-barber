70856b264bf26cf0fefb5365a71e3293726f14b7
muhzulzidan <mzulzidan@gmail.com>
2024-06-13 13:40:42 +0800

    hotfix : remove smoothscrolling

--- src/components/web/layout.tsx
+++ src/components/web/layout.tsx
@@ -1,19 +1,19 @@
 import React, { ReactNode } from 'react';
 import Header from './header';
 import Footer from './footer';
-import SmoothScrolling from "@/components/SmoothScrolling";
+// import SmoothScrolling from "@/components/SmoothScrolling";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <div className={` font-inter h-px`}>
-            <SmoothScrolling>
+            {/* <SmoothScrolling> */}
             <h1 className="hidden">Faded Lines Barber Shop</h1>
             <Header />
             <main className="">
                     {children}
             </main>
             <Footer />
-            </SmoothScrolling>
+            {/* </SmoothScrolling> */}
         </div>
     );
 };
